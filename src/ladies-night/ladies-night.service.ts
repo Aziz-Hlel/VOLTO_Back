369d5dc760d3af1520ac04f0235e7a7c9fd032d0
Tigana <tigana137@gmail.com>
2025-09-01 12:32:10 +0100

    created lafdies night

--- src/ladies-night/ladies-night.service.ts
+++ src/ladies-night/ladies-night.service.ts
@@ -1,7 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { HASHES } from 'src/redis/hashes';
+import { HASHES, HASHES3 } from 'src/redis/hashes';
 
 @Injectable()
 export class LadiesNightService {
@@ -10,23 +10,19 @@ export class LadiesNightService {
 
     static DRINK_QUOTA = 3;
 
-    async qtore(userId: string, token: string) {
-        // store with expiration
-        await this.redis.set(`refresh:${userId}`, token, 'EX', 60 * 60 * 24 * 7); // 7 days
-    }
 
 
 
     async getQrCode(userId: string) {
-        const token = await this.redis.hget(HASHES.USERS, userId);
+        const token = await this.redis.hget(HASHES.LADIES_NIGHT.USER_TO_CODE(), userId);
 
         return token;
     }
 
 
-    async getUserRemainingDrinks(userId: string) {
+    async getUserRemainingDrinks(userId: string): Promise<number> {
 
-        const remainingDrinks = await this.redis.hget(HASHES.DRINKS_CONSUMED, userId);
+        const remainingDrinks = await this.redis.hget(HASHES.LADIES_NIGHT.USER_DRINKS_CONSUMED(userId), "user_drinks_consumed");
 
         if (!remainingDrinks || !isNaN(Number(remainingDrinks))) return LadiesNightService.DRINK_QUOTA;
 
@@ -36,25 +32,71 @@ export class LadiesNightService {
 
         return nbrRemainingDrinks;
 
-    }
+    };
 
 
 
-    async generateCode() {
-        const existingCodes = await this.redis.hkeys(HASHES.QR_CODE);
+    async getUserDrinksConsumed(userId: string): Promise<number> {
+
+        const remainingDrinks = await this.redis.hget(HASHES.LADIES_NIGHT.USER_DRINKS_CONSUMED(userId), "user_drinks_consumed");
+
+        if (!remainingDrinks || !isNaN(Number(remainingDrinks))) return 0;
+
+        if (Number(remainingDrinks) > LadiesNightService.DRINK_QUOTA) {
+            // ! add log error here to get notify if this happen because it shouldnt 
+            return LadiesNightService.DRINK_QUOTA;
+        }
+
+
+        return Number(remainingDrinks);
+
+
+
+    };
+
+    async generateCode(): Promise<string> {
+
+        const existingCodes = await this.redis.hkeys(HASHES.LADIES_NIGHT.CODE_TO_USER());
         const existingCodesSet = new Set(existingCodes);
         const code = this.generateUniqueCode(existingCodesSet);
 
-        
+        return code;
+
+    }
 
 
+    async getCode(userId: string): Promise<string> {
+
+        const userRemainingDrinks = await this.getUserRemainingDrinks(userId);
+
+        if (userRemainingDrinks < 0) throw new BadRequestException('No more drinks for this user');
+
+        const existingCode = await this.redis.hget(HASHES.LADIES_NIGHT.USER_TO_CODE(), userId);
+
+        if (existingCode) return existingCode;
+
+        const code = await this.generateCode();
+
+        await this.redis.hset(HASHES.LADIES_NIGHT.USER_TO_CODE(), userId, code);
+        await this.redis.hset(HASHES.LADIES_NIGHT.CODE_TO_USER(), code, userId);
+
+        return code;
     }
 
 
 
 
+    async consumeDrink(userId: string) {
+
+        const userDrinksConsumed = await this.getUserDrinksConsumed(userId);
+
+        if (userDrinksConsumed > LadiesNightService.DRINK_QUOTA) throw new BadRequestException('No more drinks for this user');
+
+        await this.redis.hset(HASHES.LADIES_NIGHT.USER_DRINKS_CONSUMED(userId), "user_drinks_consumed", userDrinksConsumed + 1);
 
 
+
+    }
 
 
 
@@ -70,5 +112,10 @@ export class LadiesNightService {
         } while (existingCodes.has(code));
 
         return code;
-    }
+    };
+
+
+
+
+
 }
--- src/redis/hashes.ts
+++ src/redis/hashes.ts
@@ -4,13 +4,23 @@ const SESSIONS = "SESSIONS";
 const DRINKS_CONSUMED = "DRINKS_CONSUMED";
 const QR_CODE = "QR_CODE";
 
-export const HASHES = {
+export const HASHES3 = {
     USERS,
     SESSIONS,
     DRINKS_CONSUMED,
     QR_CODE
-    
+
 } as const
 
 
-export type HASHES = keyof typeof HASHES;
+export const HASHES = {
+    USER_SESSION: (userId: string) => `user:${userId}:session`,
+    LADIES_NIGHT: {
+        CODE_TO_USER: () => "LADIES_NIGHT:CODE_TO_USER",
+        USER_TO_CODE: () => "LADIES_NIGHT:USER_TO_CODE",
+        USER_DRINKS_CONSUMED: (userId: string) => `ladies_night:${userId}:user_drinks_consumed`,
+    },
+
+} as const
+
+export type HASHES3 = keyof typeof HASHES3;
