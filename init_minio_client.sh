0abee96d24bfd207e9582d38e657b193e6f06ce2
Tigana <tigana137@gmail.com>
2025-09-09 12:56:29 +0100

    add minio initilaizer

--- init_minio_client.sh
+++ init_minio_client.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+
 echo "Waiting for MinIO to be ready..."
 until mc alias set local http://minio:${MINIO_PORT} "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"; do
   echo "Retrying..."
@@ -14,7 +15,7 @@ echo "Setting public download access for \"$MINIO_BUCKET\"..."
 mc anonymous set download local/$MINIO_BUCKET
 
 echo "Uploading seed files..."
-mc cp --recursive /seed/* local/$MINIO_BUCKET/ || echo "No seed files found."
+mc cp --recursive ./seed/* local/$MINIO_BUCKET/events/ || echo "No seed files found when uploading seed files to minio❗❗❗"
 
 echo "Initialization complete."
 
