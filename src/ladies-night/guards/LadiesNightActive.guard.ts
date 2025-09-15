1306803d8ade43728fff4d2693d7613363dc52c0
Tigana <tigana137@gmail.com>
2025-09-15 17:57:58 +0100

    changed to ws exception

--- src/ladies-night/guards/LadiesNightActive.guard.ts
+++ src/ladies-night/guards/LadiesNightActive.guard.ts
@@ -1,23 +1,33 @@
 import {
   CanActivate,
   ExecutionContext,
-  ForbiddenException,
   Injectable,
 } from '@nestjs/common';
 import { LadiesNightService } from '../ladies-night.service';
-import { WsException } from '@nestjs/websockets/errors/ws-exception';
+import { Socket } from 'socket.io';
 
 @Injectable()
 export class LadiesNightActiveGuard implements CanActivate {
   constructor(private readonly ladiesNightService: LadiesNightService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // const client = context.switchToWs().getClient();
+    const client = context.switchToWs().getClient<Socket>();
+    const eventName = context.switchToWs().getPattern();
+
+    const returnEvent = {
+      "consume-drink": "drink-consumed",
+      "get-quota": "drink-quota",
+      "generate-code": "get-code",
+    }
+
     const isLadiesNightActive =
       await this.ladiesNightService.isLadiesNightActive2();
 
     if (!isLadiesNightActive)
-      throw new WsException('Ladies Night is not active right now');
+      client.emit(returnEvent[eventName], {
+        success: false,
+        message: 'Ladies Night is not active',
+      });
 
     return true;
   }
